1df992f32a524ebb93701ae858c4826444dd9b06
Antoine Balestrat <merkil@savhon.org>
2012-07-17 10:54:10 +0200

    Forgot 2 new source files.

---
+++ integer.c
@@ -0,0 +1,50 @@
+/* C Code Generator
+ *
+ * Copyright (C) 2012, Antoine Balestrat <merkil@savhon.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "ccg.h"
+
+static char const * const inttype2varid[_inttypemax] = {"c", "uc", "s", "us", "i", "ui"};
+char const * const inttype2str[_inttypemax] = {"int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t", "uint32_t"};
+
+static char *makeIntegerName(IntegerType type, VariableList *scope)
+{
+	char buffer[16] = {0}, *ret;
+
+	sprintf(buffer, "%s_%zu", inttype2varid[type], numVariablesInScope(scope));
+	ret = xmalloc(strlen(buffer) + 1);
+	strcpy(ret, buffer);
+
+	return ret;
+}
+
+void makeInteger(Variable *var, VariableList *scope)
+{
+	var->intvar.type = rand() % _inttypemax;
+    var->intvar.initializer = makeIntegerConstant(INTEGERTYPE_SIZE(var->intvar.type));
+    var->name = makeIntegerName(var->intvar.type, scope);
+}
+
+void printIntegerDecl(Variable *var)
+{
+	printf("%s %s = %s;\n", inttype2str[var->intvar.type], var->name, var->intvar.initializer.value);
+}
---
+++ pointer.c
@@ -0,0 +1,124 @@
+/* C Code Generator
+ *
+ * Copyright (C) 2012, Antoine Balestrat <merkil@savhon.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "ccg.h"
+
+#define VARIABLE_IS_NOT_POINTABLE(var) (pointerDepth(var) >= cmdline.max_pointer_depth)
+
+static char *makePointerName(VariableList *scope)
+{
+	char *ret, buff[16];
+
+	sprintf(buff, "ptr_%zu", numVariablesInScope(scope));
+	ret = xmalloc(strlen(buff) + 1);
+	strcpy(ret, buff);
+
+	return ret;
+}
+
+void makePointer(Variable *var, VariableList *scope)
+{
+	var->pointer.pointed = pickPointableVariable(scope);
+	var->name = makePointerName(scope);
+}
+
+Variable *pickPointableVariable(VariableList *scope)
+{
+	VariableList *v;
+	Variable *ret = NULL;
+	size_t n = numVariablesInScope(scope);
+
+	do
+	{
+		size_t t = rand() % n, i = 0;
+
+		foreach(v, scope)
+		{
+			if(i++ == t)
+				ret = v->variable;
+		}
+
+	} while(VARIABLE_IS_NOT_POINTABLE(ret));
+
+	return ret;
+}
+
+inline size_t pointerDepth(Variable *var)
+{
+    size_t n = 0;
+
+    while(var->type == _pointer)
+    {
+    	++n;
+    	var = var->pointer.pointed;
+    }
+
+    return n;
+}
+
+inline bool pointersInScope(VariableList *scope)
+{
+	VariableList *v;
+
+	foreach(v, scope)
+		if(v->variable->type == _pointer)
+			return true;
+
+	return false;
+}
+
+/* This function goes through all the pointers to find the type of the ultimately pointed integer ! */
+IntegerType ultimateType(Variable *var)
+{
+    while(var->type == _pointer)
+        var = var->pointer.pointed;
+
+    return var->intvar.type;
+}
+
+char *maxDerefdPointer(Variable *var)
+{
+	static char buffer[32];
+	size_t i, depth = pointerDepth(var);
+
+	memset(buffer, 0, sizeof(buffer));
+
+	for(i = 0; i < depth; ++i)
+		buffer[i] = '*';
+
+	strcat(buffer, var->name);
+
+	return buffer;
+}
+
+void printPointerDecl(Variable *var)
+{
+	size_t i, depth = pointerDepth(var);
+
+	printf("%s ", inttype2str[ultimateType(var)]);
+
+	for(i = 0; i < depth; ++i)
+		putchar('*');
+
+	printf("%s = &%s;\n", var->name, var->pointer.pointed->name);
+}
